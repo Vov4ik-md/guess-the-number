9d86f81f511c1b4d45c4dece35e5b4f21db48835
Vov4ik-md <139490040+Vov4ik-md@users.noreply.github.com>
2023-10-12 02:25:46 +0300

    addvList to check uton

--- script.js
+++ script.js
@@ -1,65 +1,18 @@
-/*console.log(document.getElementById('myHeader'))
-const myElement = document.getElementsByClassName('btn tryagain')
-console.log(myElement, 'my element')
-const allButtons = document.getElementsByTagName('button')
-console.log(allButtons, 'all buttons')
-const mytext = document.querySelector('.text')
-console.log(mytext, 'my text')
-mytext.textContent = 'Incepe a ghici'
-document.querySelector('.tryagain').setAttribute('title', 'try again')
-document.getElementsByTagName('h1').innerText = 'Ghiceste numarul'
-document.querySelector('h1').textContent = 'Ghiceste numarul'
-const myNewEl = document.createElement('div')
-myNewEl.textContent = 'ai 20 de incercari'
-console.log(myNewEl)
-const beforeEl = document.querySelector('.label-score')
-document.querySelector('.right').insertBefore(myNewEl, beforeEl)*/
+//  Afisam numarul introdus manual in urma tastarii butonului check!
+let clckBtn = document.querySelector(".check");
 
+let randNum = Math.floor((Math.random() * 20) + 1)
 
-//Manipulating CSS
-
-/*const buttonAgain = document.querySelector('.tryagain')
-
-if (buttonAgain.innerText === 'Again') {
-	buttonAgain.style.backgroundColor = 'green'
-} else {
-	buttonAgain.style.backgroundColor = 'blue
-}*/
-
-//cssText
-
-//document.querySelector('h1').style.cssText = 'font-family: Sans-serif; color: red; font-size: 16px'
-
-//  ClassList
-
-//  Events
-function chgCol() {
-	selectNumber.style.backgroundColor = 'red'
-}
-
-let selectNumber = document.querySelector('.number')
-//prindem eventul mouseEnter si schimbam culoarea in rosu
-selectNumber.addEventListener('mouseenter', chgCol)
-//capturam clickul pe el si chemam functie externa
-selectNumber.addEventListener('click', function () {
-	afisat('click')
-	selectNumber.removeEventListener('mouseenter', chgCol)
+clckBtn.addEventListener('click', function () {
+	let userInput = document.querySelector(".guess").value;
+	if (userInput > 1 && userInput <= 20 && userInput == randNum) {
+		document.querySelector(".number").textContent = userInput
+		document.querySelector(".text").innerHTML = "Bingo"
+	} else if (userInput > randNum) {
+		document.querySelector('.text').innerHTML = "To high"
+	} else if (userInput < randNum) {
+		document.querySelector(".text").innerHTML = "To low"
+	} else {
+		console.log('Introduceti numar intre 1 si 20')
+	}
 })
-// salvam functia aparte
-
-function afisat(a) {
-	selectNumber.textContent = a
-}
-
-//capturam iesirea mouse-ului din element si returnam valorile initiale
-
-const culoareaInitiala = selectNumber.style.backgroundColor = 'white'
-
-selectNumber.addEventListener('mouseleave', function () {
-	selectNumber.style.backgroundColor = culoareaInitiala
-	selectNumber.textContent = '?'
-})
-
-//remove event Listner
-
-//selectNumber.removeEventListener('mouseenter', chgCol)
